ec69864bf6a3f43e22b14006247362d2d9a1746d
Tobias Johansson <tobias@noq.nu>
2025-04-01 14:20:46 +0200

    Added Employee class with properties

---
+++ LexiconÖvning1/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconÖvning1
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public Employee(int id)
+        {
+            this.Id = id;
+        }
+        public Employee() { }
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Salary: {Salary}";
+        }
+    }
+}
